5af1510babd4dc41a44c249e073e99fca197ac87
Rob Anderson <Mctalian@users.noreply.github.com>
2025-07-27 00:50:23 -0600

    stub dragonflight raid token files

---
+++ TokenTransmogTooltips/Raids/Aberrus/tokens.lua
@@ -0,0 +1,195 @@
+local addonName, ns = ...;
+
+ns.Raids.Aberrus = {
+  --#region Dreadful (Death Knight, Demon Hunter, Warlock)
+  -- https://www.wowhead.com/item=202631/dreadful-ventilation-fluid
+  [202631] = { -- Chest
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=202624/dreadful-mixing-fluid
+  [202624] = { -- Hands
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=202634/dreadful-cooling-fluid
+  [202634] = { -- Legs
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=202621/dreadful-corrupting-fluid
+  [202621] = { -- Shoulders
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=202627/dreadful-melting-fluid
+  [202627] = { -- Head
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  --#endregion
+
+  --#region Mystic (Druid, Hunter, Mage)
+  -- https://www.wowhead.com/item=202632/mystic-ventilation-fluid
+  [202632] = { -- Chest
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=202625/mystic-mixing-fluid
+  [202625] = { -- Hands
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=202635/mystic-cooling-fluid
+  [202635] = { -- Legs
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=202622/mystic-corrupting-fluid
+  [202622] = { -- Shoulders
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=202628/mystic-melting-fluid
+  [202628] = { -- Head
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  --#endregion
+
+  --#region Venerated (Paladin, Priest, Shaman)
+  -- https://www.wowhead.com/item=202633/venerated-ventilation-fluid
+  [202633] = { -- Chest
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=202626/venerated-mixing-fluid
+  [202626] = { -- Hands
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=202636/venerated-cooling-fluid
+  [202636] = { -- Legs
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=202623/venerated-corrupting-fluid
+  [202623] = { -- Shoulders
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=202629/venerated-melting-fluid
+  [202629] = { -- Head
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  --#endregion
+
+  --#region Zenith (Evoker, Monk, Rogue, Warrior)
+  -- https://www.wowhead.com/item=202639/zenith-ventilation-fluid
+  [202639] = { -- Chest
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=202638/zenith-mixing-fluid
+  [202638] = { -- Hands
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=202640/zenith-cooling-fluid
+  [202640] = { -- Legs
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=202637/zenith-corrupting-fluid
+  [202637] = { -- Shoulders
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=202630/zenith-melting-fluid
+  [202630] = { -- Head
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  --#endregion
+}
---
+++ TokenTransmogTooltips/Raids/Amirdrassil/tokens.lua
@@ -0,0 +1,195 @@
+local addonName, ns = ...;
+
+ns.Raids.Amirdrassil = {
+  --#region Dreadful (Death Knight, Demon Hunter, Warlock)
+  -- https://www.wowhead.com/item=207462/dreadful-verdurous-dreamheart
+  [207462] = { -- Chest
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=207466/dreadful-tormented-dreamheart
+  [207466] = { -- Hands
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=207474/dreadful-ashen-dreamheart
+  [207474] = { -- Legs
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=207478/dreadful-smoldering-dreamheart
+  [207478] = { -- Shoulders
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=207470/dreadful-blazing-dreamheart
+  [207470] = { -- Head
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  --#endregion
+
+  --#region Mystic (Druid, Hunter, Mage)
+  -- https://www.wowhead.com/item=207463/mystic-verdurous-dreamheart
+  [207463] = { -- Chest
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=207467/mystic-tormented-dreamheart
+  [207467] = { -- Hands
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=207475/mystic-ashen-dreamheart
+  [207475] = { -- Legs
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=207479/mystic-smoldering-dreamheart
+  [207479] = { -- Shoulders
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=207471/mystic-blazing-dreamheart
+  [207471] = { -- Head
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  --#endregion
+
+  --#region Venerated (Paladin, Priest, Shaman)
+  -- https://www.wowhead.com/item=207464/venerated-verdurous-dreamheart
+  [207464] = { -- Chest
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=207468/venerated-tormented-dreamheart
+  [207468] = { -- Hands
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=207476/venerated-ashen-dreamheart
+  [207476] = { -- Legs
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=207480/venerated-smoldering-dreamheart
+  [207480] = { -- Shoulders
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=207472/venerated-blazing-dreamheart
+  [207472] = { -- Head
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  --#endregion
+
+  --#region Zenith (Evoker, Monk, Rogue, Warrior)
+  -- https://www.wowhead.com/item=207465/zenith-verdurous-dreamheart
+  [207465] = { -- Chest
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=207469/zenith-tormented-dreamheart
+  [207469] = { -- Hands
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=207477/zenith-ashen-dreamheart
+  [207477] = { -- Legs
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=207481/zenith-smoldering-dreamheart
+  [207481] = { -- Shoulders
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  -- https://www.wowhead.com/item=207473/zenith-blazing-dreamheart
+  [207473] = { -- Head
+    Difficulties = {
+      [Enum.ItemCreationContext.RaidFinder] = {},
+      [Enum.ItemCreationContext.RaidNormal] = {},
+      [Enum.ItemCreationContext.RaidHeroic] = {},
+      [Enum.ItemCreationContext.RaidMythic] = {}
+    }
+  },
+  --#endregion
+}
